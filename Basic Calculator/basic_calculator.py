8357f74697d4e578370dd60986bd43bb25bca2f5
ItchiSushi <dpengmarais99@gmail.com>
2024-09-13 13:37:11 +0200

    Created Basic Calculator App

---
+++ Basic Calculator/basic_calculator.py
@@ -0,0 +1,5 @@
+# Start by defining the functions needed to calculate the area of a circle such as add, subtract, divide, multiply
+# Now we print the options for the use to choose from
+# Ask the user for the values
+# Call the appropriate functions
+# Create a while loop to continue the program until the user wants to exit
