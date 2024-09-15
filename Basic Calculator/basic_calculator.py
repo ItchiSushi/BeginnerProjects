ccefed07f8d04762f9707433f061803cbeaca2a4
ItchiSushi <dpengmarais99@gmail.com>
2024-09-15 13:53:07 +0200

    Continued working on the calculator app.

--- Basic Calculator/basic_calculator.py
+++ Basic Calculator/basic_calculator.py
@@ -3,3 +3,35 @@
 # Ask the user for the values
 # Call the appropriate functions
 # Create a while loop to continue the program until the user wants to exit
+
+def add(a, b):
+    answer = a + b
+    print(str(a) + " + " + str(b) + " = " + str(answer))
+
+def subtract(a, b):
+    answer = a - b
+    print(str(a) + " - " + str(b) + " = " + str(answer))
+
+def divide(a, b):
+    answer = a / b
+    print(str(a) + " ÷ " + str(b) + " = " + str(answer))
+
+def multiply(a, b):
+    answer = a * b
+    print(str(a) + " x " + str(b) + " = " + str(answer))
+
+user_answer = "Yes"
+
+while user_answer:
+    user_question = input("""
+    Which Mathematics application would you like to use?
+    
+    a) Addition
+    b) Subtraction
+    c) Division
+    d) Multiplication
+    
+    Choose a, b, c, or d: """)
+
+
+
