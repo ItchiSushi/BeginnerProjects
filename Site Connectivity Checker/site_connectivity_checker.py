32a6d09afa60f72766feae0182439b5a5dba9fbd
ItchiSushi <dpengmarais99@gmail.com>
2024-09-18 15:31:16 +0200

    Created and completed site connectivity checker program.

---
+++ Site Connectivity Checker/site_connectivity_checker.py
@@ -0,0 +1,8 @@
+# import urllib
+# Use urllib.request to get the data from the url
+# Write a function that takes accepts url
+# Returns a response
+
+import urllib.request as urllib
+
+def main(url)
